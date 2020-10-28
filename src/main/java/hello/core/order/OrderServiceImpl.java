5ccc47f327d7c9aae6d00ab16609c113149f9223
yjs2952 <yjs2952@naver.com>
2020-10-29 00:53:20 +0900

    새로운 할인 정책 적용과 문제점

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,7 +1,6 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,9 +8,13 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();    // OrderServiceImpl 이 인터페이스(DiscountPolicy)에만 의존하는게 아니라 구체적인 클래스(FixDiscountPolicy)도 의존하고 있다 => DIP 위반
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();     // OrderServiceImpl 이 FixDiscountPolicy 에서 RateDiscountPolicy 로 의존성을 변경하려면 OrderServiceImpl 코드를 직접 수정해야 한다. => OCP 위반
+
+    private DiscountPolicy discountPolicy;
 
     @Override
+
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
         int discountPrice = discountPolicy.discount(member, itemPrice);
