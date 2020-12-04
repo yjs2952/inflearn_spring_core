482b5fff3117e7a3da0e31715137d418b17af975
yjs2952 <yjs2952@naver.com>
2020-12-04 21:15:37 +0900

    @Configuration 과 싱글톤

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -24,4 +24,9 @@ public class OrderServiceImpl implements OrderService {
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
+
+    // test 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
