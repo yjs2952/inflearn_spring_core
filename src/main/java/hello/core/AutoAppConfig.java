1ff916bbc3740a69797fba87a6465f63ad9eb7da
yjs2952 <yjs2952@naver.com>
2020-12-05 22:01:05 +0900

    탐색 위치와 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core.member",
         // @Configuration 이 붙은 클래스도 @Component 가 붙어있어 컨포넌트 스캔 대상이 되기 때문에 빈 충돌 우려가 있다.
         // 이러한 경우를 방지하기 위해 스캔 대상에서 제외한다.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
