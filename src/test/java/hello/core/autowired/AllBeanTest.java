5c4b7fc7b03b9cbd80b4fc5873f39f7c74f57808
yjs2952 <yjs2952@naver.com>
2020-12-10 23:47:56 +0900

    빈 생명주기 콜백 시작

--- src/test/java/hello/core/autowired/AllBeanTest.java
+++ src/test/java/hello/core/autowired/AllBeanTest.java
@@ -5,7 +5,6 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
