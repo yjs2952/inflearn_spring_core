8b3c52c305d21298a956a489de10bdc8e2d57814
yjs2952 <yjs2952@naver.com>
2020-12-12 18:22:59 +0900

    인터페이스 InitializingBean, DisposableBean

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,14 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -28,4 +29,26 @@ public class NetworkClient {
         System.out.println("close: " + url);
     }
 
+    /*
+    InitializingBean 에서 제공하는 메소드
+    스프링 전용 인터페이스
+    초기화 콜백 : 빈이 생성되고, 빈의 의존관계 주입이 완료된 후 호출
+     */
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    /*
+    DisposableBean 에서 제공하는 메소드
+    스프링 전용 인터페이스
+    소멸전 콜백 : 빈이 소멸되기 직전에 호출
+     */
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
+    }
 }
